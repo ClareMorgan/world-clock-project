262ced05474e4ebdff99128f9882cf4b64ba5241
ClareMorgan <claredotm@gmail.com>
2024-03-29 14:07:16 +0000

    Added option groups to select

--- src/index.js
+++ src/index.js
@@ -30,29 +30,35 @@ function updateTime() {
     "HH:mm:ss[<small>]A[</small>]"
   );
 }
-updateTime();
-setInterval(updateTime, 1000);
 
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
+function updateCity() {
+  if (!selectedCity) return;
+  if (selectedCity === "current") {
+    selectedCity = moment.tz.guess();
   }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment.tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
+  let cityName = selectedCity.replace("_", " ").split("/")[1];
+  let cityDate = moment.tz(selectedCity).format("dddd MMMM Do YYYY");
+  let cityTime = moment.tz(selectedCity).format("HH:mm:ss[<small>]A[</small>]");
+  let citiesElement = document.querySelector("#cities-display");
 
   citiesElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("dddd MMMM Do YYYY")}</div>
+            <div class="date">${cityDate}</div>
           </div>
-          <div class="time">${cityTime.format(
-            "HH:mm:ss[<small>]A[</small>]"
-          )}</div>
+          <div class="time">${cityTime}</div>
         </div>
         <a href="/">Back to all cities</ a>`;
 }
 
-let citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-select");
+citiesSelectElement.addEventListener("change", function (event) {
+  selectedCity = event.target.value;
+  updateCity();
+});
+
+let cityTimeZone = null;
+setInterval(updateCity, 1000);
